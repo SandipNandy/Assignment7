9351b34af9336f61e44cfafeab1b02a126d5c21a
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-05-11 23:24:32 +0530

    Update Assignment7.java

--- Assignment7.java
+++ Assignment7.java
@@ -15,6 +15,7 @@ public class Assignment7 {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		
+		
 		driver.get("https://AutomationPractice/");
 		driver.manage().timeouts().implicitlyWait(6000, TimeUnit.SECONDS);
 		JavascriptExecutor js=(JavascriptExecutor)driver;
