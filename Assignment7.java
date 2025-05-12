81d2a91158bc57eef43095ca1a0aed5473a94d86
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-05-12 09:04:31 +0530

    Update Assignment7.java

--- Assignment7.java
+++ Assignment7.java
@@ -28,6 +28,7 @@ public class Assignment7 {
 		driver.get("https://AutomationPractice/");
 		
 		driver.manage().timeouts().implicitlyWait(6000, TimeUnit.SECONDS);
+		
 		JavascriptExecutor js=(JavascriptExecutor)driver;
 		js.executeScript("window.scrollBy(0,500)");
 		
