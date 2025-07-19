efaea73f390521c84536559eb768df90fa8e87b0
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-07-19 14:53:10 +0530

    Update Assignment7.java

--- Assignment7.java
+++ Assignment7.java
@@ -13,7 +13,6 @@ public class Assignment7 {
 		
 		System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		
 		driver.manage().window().maximize();
 		
 		
