467abd385c7da0a73ceaa1aae16e7a973e3806cc
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2026-01-15 19:16:07 +0530

    Format and clean up Assignment7.java

--- Assignment7.java
+++ Assignment7.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
@@ -61,6 +60,7 @@ public class Assignment7 {
 	}
 
 }
+
 
 
 
