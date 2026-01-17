a2fc93ba5cbdd949acfee4bc6eb2867d3d61b944
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2026-01-17 08:36:12 +0530

    Format and clean up Assignment7.java

--- Assignment7.java
+++ Assignment7.java
@@ -16,7 +16,6 @@ public class Assignment7 {
 		driver.get("https://AutomationPractice/");
 		driver.manage().timeouts().implicitlyWait(6000, TimeUnit.SECONDS);
 		JavascriptExecutor js=(JavascriptExecutor)driver;
-		
 		js.executeScript("window.scrollBy(0,500)");
 		
 		List<WebElement> HeadingValues=driver.findElements(By.cssSelector(".table-display th:nth-child(2)"));
@@ -57,6 +56,7 @@ public class Assignment7 {
 	}
 
 }
+
 
 
 
